aa62bd52bcf09bedd2f8656cc59dce6accc04865
diegoalvescf <dalvesdocs@gmail.com>
2023-04-17 18:11:40 -0300

    refactor: adjust in metro

--- metro.config.js
+++ metro.config.js
@@ -1,19 +1,23 @@
 const { getDefaultConfig } = require('expo/metro-config')
 
-module.exports = (() => {
-    const config = getDefaultConfig(__dirname)
+module.exports = (async () => {
+    const {
+        resolver: { sourceExts },
+    } = await getDefaultConfig(__dirname)
 
-    const { transformer, resolver } = config
-
-    config.transformer = {
-        ...transformer,
-        babelTransformerPath: require.resolve('react-native-svg-transformer'),
+    return {
+        transformer: {
+            getTransformOptions: async () => ({
+                transform: {
+                    experimentalImportSupport: false,
+                    inlineRequires: false,
+                },
+            }),
+            babelTransformerPath: require.resolve('react-native-svg-transformer'),
+        },
+        resolver: {
+            assetExts: ['cjs', 'bmp', 'gif', 'jpg', 'jpeg', 'png', 'psd', 'svg', 'webp'],
+            sourceExts: [...sourceExts, 'svg', 'json', 'jsx', 'js', 'ts', 'tsx', 'cjs'],
+        },
     }
-    config.resolver = {
-        ...resolver,
-        assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
-        sourceExts: [...resolver.sourceExts, 'svg'],
-    }
-
-    return config
 })()
