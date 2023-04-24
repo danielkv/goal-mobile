92c9387544e242141cdcbb9934ec269909645302
Daniel K. Guolo <daniel_kv@hotmail.com>
2023-04-24 15:59:01 -0300

    feat:  carosel Scrollview

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -10,3 +10,11 @@ declare module '*.svg' {
     const content: React.FC<SvgProps>
     export default content
 }
+
+declare module 'react-native-new-snap-carousel' {
+    import Carousel from 'react-native-snap-carousel'
+
+    export * from 'react-native-snap-carousel'
+
+    export default Carousel
+}
--- src/view/SectionCarouselScreen/index.tsx
+++ src/view/SectionCarouselScreen/index.tsx
@@ -1,7 +1,8 @@
-import { useEffect } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { Dimensions } from 'react-native'
+import Carousel, { Pagination } from 'react-native-new-snap-carousel'
 
-import { Box, Flex, useTheme } from 'native-base'
+import { Box, Flex, ScrollView, useTheme } from 'native-base'
 
 import SectionItem from './components/SectionItem'
 import { IFlatSection } from '@common/interfaces/worksheet'
@@ -9,14 +10,14 @@ import ActivityIndicator from '@components/ActivityIndicator'
 import AlertBox from '@components/AlertBox'
 import { RouteProp, useNavigation, useRoute } from '@react-navigation/native'
 import { TReactNavigationStackParamList } from '@router/types'
-import { FlashList } from '@shopify/flash-list'
 import { getWorksheetDayByIdFnUseCase } from '@useCases/worksheet/getWorksheetDayById'
 import { getErrorMessage } from '@utils/getErrorMessage'
 import { capitalize } from '@utils/strings'
 import dayjs from 'dayjs'
 import useSWR from 'swr'
 
-const SECTION_CARD_WIDTH = Dimensions.get('window').width * 0.8
+const SCREEN_WIDTH = Dimensions.get('window').width
+const SECTION_CARD_WIDTH = SCREEN_WIDTH * 0.8
 const SECTION_CARD_MARGIN = 17
 
 const SectionCarouselScreen: React.FC = () => {
@@ -26,6 +27,8 @@ const SectionCarouselScreen: React.FC = () => {
         params: { worksheetId, dayId, sectionIndex },
     } = useRoute<RouteProp<TReactNavigationStackParamList, 'SectionCarousel'>>()
     const navigation = useNavigation()
+
+    const [activeSlide, setActiveSlide] = useState(sectionIndex)
 
     const { data, isLoading, error } = useSWR([worksheetId, dayId, 'worksheetDay'], (args: string[]) =>
         getWorksheetDayByIdFnUseCase(args[0], args[1])
@@ -46,33 +49,47 @@ const SectionCarouselScreen: React.FC = () => {
             </Flex>
         )
 
-    const sections =
-        data?.periods.flatMap<IFlatSection>((periods, periodIndex) =>
-            periods.sections.map((section, sectionIndex) => ({
-                period: periodIndex + 1,
-                sectionNumber: sectionIndex + 1,
-                ...section,
-            }))
-        ) || []
+    const sections = useMemo(
+        () =>
+            data?.periods.flatMap<IFlatSection>((periods, periodIndex) =>
+                periods.sections.map((section, sectionIndex) => ({
+                    period: periodIndex + 1,
+                    sectionNumber: sectionIndex + 1,
+                    ...section,
+                }))
+            ) || [],
+        []
+    )
 
     return (
-        <FlashList
-            data={sections}
-            horizontal
-            initialScrollIndex={sectionIndex}
-            renderItem={({ item, index }) => (
-                <Box m={`${SECTION_CARD_MARGIN}px`}>
-                    <SectionItem width={SECTION_CARD_WIDTH} item={item} />
-                </Box>
-            )}
-            snapToInterval={SECTION_CARD_WIDTH + SECTION_CARD_MARGIN * 2}
-            showsHorizontalScrollIndicator={false}
-            estimatedItemSize={SECTION_CARD_WIDTH + SECTION_CARD_MARGIN * 2}
-            contentContainerStyle={{
-                paddingVertical: sizes[7],
-                paddingHorizontal: sizes[4],
-            }}
-        />
+        <Box flexGrow={1}>
+            <Pagination
+                dotsLength={sections.length}
+                activeDotIndex={activeSlide}
+                containerStyle={{ paddingVertical: sizes[3] }}
+                dotStyle={{
+                    width: 8,
+                    height: 8,
+                    backgroundColor: 'white',
+                }}
+                inactiveDotOpacity={0.4}
+                inactiveDotScale={0.5}
+            />
+            <Carousel
+                data={sections}
+                renderItem={({ item }) => (
+                    <Box mx={`${SECTION_CARD_MARGIN}px`}>
+                        <ScrollView>
+                            <SectionItem width={SECTION_CARD_WIDTH} item={item} />
+                        </ScrollView>
+                    </Box>
+                )}
+                layout="default"
+                onSnapToItem={(index) => setActiveSlide(index)}
+                sliderWidth={SCREEN_WIDTH}
+                itemWidth={SECTION_CARD_WIDTH + SECTION_CARD_MARGIN * 2}
+            />
+        </Box>
     )
 }
 
